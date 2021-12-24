60809a77e7fdd81dd09da2d64773fbd2beb1ba5a
Emil <emilbabazade29@gmail.com>
2021-12-24 16:46:43 +0400

    deleting terminal order with delete_model

--- src/app/components/terminal-services/services/services.component.ts
+++ src/app/components/terminal-services/services/services.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import TerminalItem from 'src/app/model/terminal-item';
 import { TerminalService } from 'src/app/services/terminal.service';
+import { DeleteDialogComponent } from '../../delete-dialog/delete-dialog.component';
 
 @Component({
     selector: 'app-services',
@@ -15,9 +17,49 @@ export class ServicesComponent implements OnInit {
     dataSource: MatTableDataSource<TerminalItem> = new MatTableDataSource<TerminalItem>(this.terminalItems);
     @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-    constructor(private terminalService: TerminalService,) { }
+    constructor(
+        private terminalService: TerminalService,
+        public dialog: MatDialog
+    ) { }
 
-    ngOnInit(): void {
+    ngOnInit() {
+        this.getTerminalorders();
+    }
+
+    openDialog(orderId: number, orderNo: string) {
+        const dialogRef = this.dialog.open(DeleteDialogComponent, {
+            width: '250px',
+            data: {orderNo, orderId},
+        });
+
+        dialogRef.afterClosed().subscribe(orderId => {
+            if(!orderId) {
+                return;
+            }
+            this.deleteTerminalOrder(orderId);
+        });
+    }
+
+    private deleteTerminalOrder(orderId: number) {
+        this.terminalService
+            .deleteTerminalOrder(orderId)
+            .subscribe(response => {
+                switch(response.status) {
+                case 404:
+                    //TODO: popup
+                    console.log('NOT FOUND');
+                    break;
+                case 400: case 500:
+                    //TODO: popup
+                    console.log('ERROR');
+                    break;
+                default:
+                    this.getTerminalorders();
+                }
+            });
+    }
+
+    private getTerminalorders() {
         this.terminalService
             .getTerminalOrders()
             .subscribe(terminalItems => {
