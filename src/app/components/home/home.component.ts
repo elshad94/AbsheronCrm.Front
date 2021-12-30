caa0d16d477c5599218a516f39544250af8c066b
Emil <emilbabazade29@gmail.com>
2021-12-30 15:47:11 +0400

    minor bugs

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,77 +16,6 @@ export class HomeComponent implements OnInit {
 
     conWag: ConWag[] = [];
 
-    // ngAfterViewInit() {
-    //   this.orderCount.getOrdersCount().subscribe((data: ConWag[]) => {
-    //     this.conWag = data;
-    //     console.log(data)
-    //   });
-    //   this.d1.nativeElement.insertAdjacentHTML('beforeend', `<canvas id="updatingData" style="height: 20rem;"
-    //   data-hs-chartjs-options='{
-
-    //     "type": "bar",
-    //     "data": {
-    //       "labels": ["Yan", "Fev", "Mar", "Apr", "May", "İyn", "İyl", "Avq", "Sen", "Okt","Noy","Dek"],
-    //       "datasets": [{
-    //         "data": [${this.conWag.map(x => x.konteynrCount)}],
-    //         "backgroundColor": "#57DAC2",
-    //         "hoverBackgroundColor": "#57DAC2",
-    //         "borderColor": "#57DAC2"
-    //       },
-    //       {
-    //         "data": [${this.conWag.map(x => x.vaqonCount)}],
-    //         "backgroundColor": "#377DFF",
-    //         "borderColor": "#377DFF"
-    //       }]
-    //     },
-    //     "options": {
-    //       "scales": {
-    //         "yAxes": [{
-    //           "gridLines": {
-    //             "color": "#e7eaf3",
-    //             "drawBorder": false,
-    //             "zeroLineColor": "#e7eaf3"
-    //           },
-    //           "ticks": {
-    //             "beginAtZero": true,
-    //             "stepSize": 100,
-    //             "fontSize": 12,
-    //             "fontColor": "#000000",
-
-    //             "fontFamily": "Open Sans, sans-serif",
-    //             "padding": 20,
-    //             "postfix": ""
-    //           }
-    //         }],
-    //         "xAxes": [{
-    //           "gridLines": {
-    //             "display": false,
-    //             "drawBorder": false
-    //           },
-    //           "ticks": {
-    //             "fontSize": 12,
-    //             "fontColor": "#97a4af",
-    //             "fontFamily": "Open Sans, sans-serif",
-    //             "padding": 10
-    //           },
-    //           "categoryPercentage": 0.5,
-    //           "maxBarThickness": "12"
-    //         }]
-    //       },
-    //       "cornerRadius": 2,
-    //       "tooltips": {
-    //         "prefix": "",
-    //         "hasIndicator": true,
-    //         "mode": "index",
-    //         "intersect": false
-    //       },
-    //       "hover": {
-    //         "mode": "nearest",
-    //         "intersect": true
-    //       }
-    //     }
-    //   }'></canvas>`);
-    // }
     public barChartOptions: ChartConfiguration['options'] = {
         responsive: true,
         scales: {
@@ -97,7 +26,7 @@ export class HomeComponent implements OnInit {
                     lineWidth: 0,
                 },
                 ticks: {
-                    color: "#000000",
+                    color: '#000000',
                     padding: 20,
                     maxTicksLimit: 7,
                 }
@@ -111,14 +40,14 @@ export class HomeComponent implements OnInit {
         plugins: {
             legend: {
                 display: false,
-                
+
             },
             datalabels: {
                 anchor: 'end',
                 align: 'end'
             },
             tooltip:{
-                mode: "index",
+                mode: 'index',
                 intersect: false
             }
         }
@@ -130,7 +59,7 @@ export class HomeComponent implements OnInit {
 
     public barChartData!: ChartData<'bar'>;
 
-    public barCharDatasetProperties!: ChartDatasetProperties<'bar', number[]>
+    public barCharDatasetProperties!: ChartDatasetProperties<'bar', number[]>;
 
     ngOnInit() {
         this.orderCount.getOrdersCount().subscribe((conwags: ConWag[]) => {
@@ -140,9 +69,8 @@ export class HomeComponent implements OnInit {
             this.barChartData = {
                 labels: conwags.map(c => c.orderMonthText),
                 datasets: [
-                    { data: conwags.map(c => c.vaqonCount), label: 'Vaqonlar' },
-
-                    { data: conwags.map(c => c.konteynrCount), label: 'Konteynerler' }
+                    { data: conwags.map(c => c.vaqonCount), label: 'Vaqonlar', backgroundColor: '#57dac2' },
+                    { data: conwags.map(c => c.konteynrCount), label: 'Konteynerler', backgroundColor: '#377dff' }
                 ]
             };
         });
