d6c469b525807fada92094a049db558e4b28235c
Remzi <remzibalaksihiyev13@gmail.com>
2022-01-28 10:34:50 +0400

    Showing validation message when user become deactive

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -54,7 +54,7 @@ export class LoginComponent implements OnInit {
         this.router.navigate(['/home']);
       },
       error:res => {
-        if(res.error.data == '1' || res.error.data == '0' ||  res.error.data=='2' || res.error.data=='3')
+        if(res.error.data == '1' || res.error.data == '0' ||  res.error.data=='2' || res.error.data=='3' || res.error.data=='4')
         {
           errorAlert(res.error.programMessage,'Xəta');
           return;
