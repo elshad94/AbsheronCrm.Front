594415f5fd23dd144db1fbfd8cbd724ce54d990b
rashadjabbar <resadc30@gmail.com>
2022-02-18 17:40:16 +0400

    regiser and user account validation

--- src/app/components/setting/profile-account/profile-account.component.ts
+++ src/app/components/setting/profile-account/profile-account.component.ts
@@ -126,7 +126,7 @@ export class ProfileAccountComponent implements OnInit {
   }
 
   phoneInput(event: any) {
-    var x = event.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,2})(\d{0,3})(\d{0,2})(\d{0,2})/);
-    event.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '') + (x[4] ? '-' + x[4] : '') + (x[5] ? '-' + x[5] : '');
+    var x = event.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
+    event.target.value = !x[2] ? x[1] :  x[1]  + (x[2] ? '-' + x[2] : '') + (x[3] ? '-' + x[3] : '') + (x[4] ? '-' + x[4] : '') + (x[5] ? '-' + x[5] : '');
   }
 }
