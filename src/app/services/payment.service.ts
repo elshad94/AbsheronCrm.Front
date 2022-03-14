01b0d3d9345fd5f3372fd9475005b7168faadee1
emilbabazade20@gmail.com <emilbabazade29@gmail.com>
2022-03-14 10:01:02 +0400

    add balance

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -5,11 +5,21 @@ import { Payments } from '../model/payments';
 import { ApiUrlsService } from './api-urls.service';
 const baseUrl = 'https://localhost:44323/api/Payment/GetAll';
 
+interface AddBalanceDTO {
+  isBrokerBalance: boolean,
+  amount: number
+}
+
+interface AddBalanceResult {
+  blogId: number,
+  url: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
-
+  blogId?: number;
   baseUrl!: string;
   constructor(private http: HttpClient, private apiUrlService: ApiUrlsService) {
     this.baseUrl = apiUrlService.getCrmAPIURI();
@@ -17,6 +27,11 @@ export class PaymentService {
 
   payAll(): Observable<Payments[]> {
     return this.http.get<Payments[]>(`${this.baseUrl}/Payment/GetAll`);
+  }
+
+  addBalance(balanceDTO: AddBalanceDTO): Observable<AddBalanceResult> {
+    return this.http
+      .post<AddBalanceResult>(`${this.baseUrl}/Payment/AddBalance`, balanceDTO);
   }
 
 }
