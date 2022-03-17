550f2d1254cc2e33c9f009beb9b83d18141e67e3
rashadjabbar <resadc30@gmail.com>
2022-03-17 16:56:55 +0400

    asdasd

--- src/app/components/terminal-services/new-order/order/order.component.ts
+++ src/app/components/terminal-services/new-order/order/order.component.ts
@@ -155,27 +155,6 @@ export class OrderComponent implements OnInit {
       expenseText: this.expenses.filter(exp => exp.id == x.expenseId)[0].text
     };});
 
-
-    const allServices = updateTerminalData.xidmetler.map(x => {return[
-        {   
-          count: x.miqdar,
-          edv: x.edv,
-          expenseId: x.expenseId,
-          totalAmount: x.cemi,
-          temrinalWay: {
-            nvNo: x.nvNo,
-            amount: x.qiymet,
-            isSelected: true,
-          },
-          expenseText: this.expenses.filter(exp => exp.id == x.expenseId)[0].text
-        }
-      ,
-    ]});
-
-    console.log(allServices)
-
-
-
     for(const x of this.xidmetler ) {
       this.totalEdv += x.edv * x.count;
     }
